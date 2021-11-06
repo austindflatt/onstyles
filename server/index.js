436d93cb82b1b282006e74c3b31bfd827029abbc
austindflatt <austindflatt@gmail.com>
2021-11-06 16:12:27 -0400

    setting up backend with MongoDB

--- server/index.js
+++ server/index.js
@@ -10,3 +10,11 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 // MongoDB
+// Cluster information
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
